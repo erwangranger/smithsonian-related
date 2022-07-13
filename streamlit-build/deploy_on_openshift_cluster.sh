e2cd13813c816d15a7472517771c0c5a18527319
Erwan Granger <32554862+erwangranger@users.noreply.github.com>
2022-07-13 15:00:21 -0400

    improvements.

--- streamlit-build/deploy_on_openshift_cluster.sh
+++ streamlit-build/deploy_on_openshift_cluster.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+NS=dap-app
+
 oc new-project dap-app
 
 oc -n dap-app\
     new-app \
     https://github.com/erwangranger/smithsonian-related.git#main \
     --context-dir=streamlit-build \
-    --docker-image=python:3.10.4 \
+    --image=python:3.10.4 \
     --strategy=docker \
-    --dry-run=true
 
-oc expose service/smithsonian-related
+
+echo 'waiting for service'
+timeout --foreground \
+    600s \
+    bash -c -- \
+        "until oc -n dap-app get service \
+        | grep -o -E 'smithsonian'  > /dev/null 2>&1; \
+        do printf '.' ; \
+        sleep 5 ; \
+        done"
+
+oc  -n dap-app expose service/smithsonian-related
 
